08d76ed73760bb93c944af9e93f44a13d945a317
KhaiHuynhVN <khaiht2000@gmail.com>
2025-12-18 17:04:01 +0700

    bật hrm để fix lỗi Angular không effect file scss khi change file scss

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,5 @@
-import { Component, inject, signal, PLATFORM_ID } from "@angular/core";
-import { isPlatformBrowser } from "@angular/common";
+import { Component, signal } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
-import { TranslateService } from "@ngx-translate/core";
 
 import { AngularHeader } from "../components";
 
@@ -13,15 +11,6 @@ import { AngularHeader } from "../components";
 })
 class App {
    protected readonly title = signal("DemonVN");
-
-   private translate = inject(TranslateService);
-   private platformId = inject(PLATFORM_ID);
-
-   constructor() {
-      if (isPlatformBrowser(this.platformId)) {
-         this.translate.reloadLang("vi");
-      }
-   }
 }
 
 export default App;
--- src/components/AngularHeader/AngularHeader.ts
+++ src/components/AngularHeader/AngularHeader.ts
@@ -1,5 +1,5 @@
-import { Component, Input } from "@angular/core";
-import { TranslateModule } from "@ngx-translate/core";
+import { Component, Input, inject } from "@angular/core";
+import { TranslateModule, TranslateService } from "@ngx-translate/core";
 
 import { Icons } from "../../assets";
 
@@ -12,6 +12,13 @@ import { Icons } from "../../assets";
 })
 class AngularHeader {
    @Input() title: string = "";
+
+   private translate = inject(TranslateService);
+
+   changeLang(event: Event) {
+      const select = event.target as HTMLSelectElement;
+      this.translate.use(select.value);
+   }
 }
 
 export default AngularHeader;
